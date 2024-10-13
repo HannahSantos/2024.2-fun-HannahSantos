0733d48e56e2343ca1e8b924766c6e4557f8b756
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-13 19:20:06 -0300

    Uncurried types on Bool.band and Bool.bor

--- haskell/Bool.hs
+++ haskell/Bool.hs
@@ -7,13 +7,13 @@ data Bool where
     False :: Bool
     deriving (Eq, Show)
 
-band :: Bool -> Bool -> Bool
-band True True = True
-band _ _ = False
+band :: (Bool, Bool) -> Bool
+band (True, True) = True
+band (_, _) = False
 
-bor :: Bool -> Bool -> Bool
-bor False False = False
-bor _ _ = True
+bor :: (Bool, Bool) -> Bool
+bor (False, False) = False
+bor (_, _) = True
 
 bnot :: Bool -> Bool
 bnot True = False

7b23bff5ce1c9e10c54018a523031ac6c60c70b5
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-15 08:47:37 -0300

    Changed some unused variables to _

--- haskell/Functions.hs
+++ haskell/Functions.hs
@@ -30,14 +30,14 @@ zero O = True
 zero _ = False
 
 if_then_else :: Bool -> a -> a -> a
-if_then_else True a b = a
-if_then_else False a b = b
+if_then_else True a _ = a
+if_then_else False _ b = b
 
 fst :: (a, b) -> a
-fst (a, b) = a
+fst (a, _) = a
 
 snd :: (a, b) -> b
-snd (a, b) = b
+snd (_, b) = b
 
 div :: (Nat, Nat) -> (Nat, Nat)
 div (_, O) = error "Zero cannot go there, your computer might break."

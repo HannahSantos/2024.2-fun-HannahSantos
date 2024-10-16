2cd430bfa1c0b324218361df39ac6d9520550016
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-16 11:02:54 -0300

    Started Polyfuns module

---
+++ haskell/Polyfuns.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+module Polyfuns where
+
+import Prelude hiding (Bool, True, False, fst, snd)
+import Bool
+
+if_then_else :: Bool -> a -> a -> a
+if_then_else True a _ = a
+if_then_else False _ b = b
+
+fst :: (a, b) -> a
+fst (a, _) = a
+
+snd :: (a, b) -> b
+snd (_, b) = b
